3a041d657452ebdd5a2ecf2892342d308d7e66c5
VadymMakohon <errorks7@gmail.com>
2024-07-01 15:42:50 +0200

    added all of the steps from the exercise

---
+++ Exersize_1/add.py
@@ -0,0 +1,6 @@
+monday_kilometers = int(input("How many kilometers did you run on Monday?"))
+tuesday_kilometers = int(input("How many kilometers did you run on Tuesday?"))
+
+total_kilometers = monday_kilometers + tuesday_kilometers
+
+print("You have run ", f"{total_kilometers:,}", " kilometers so far this week. Good job!")
